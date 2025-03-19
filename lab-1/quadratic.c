834f3e9dc1fb23e1e78f54cd48cdff483bb77adc
Baklanov-Daniil <daniilbaklanov55@gmail.com>
2025-03-19 23:28:50 +0600

    init: reuploading the thread

--- lab-1/quadratic.c
+++ lab-1/quadratic.c
@@ -1,1 +1,47 @@
-// Вычисление корней квадратного уравнения
+#include "quadratic.h"
+#include <math.h>
+
+double get_accuracy(double value, int accuracy) {
+  double pow_10 = pow(10, accuracy);
+  return round(value * pow_10) / pow_10;
+}
+
+int sign(double value) {
+  if (value > 0)
+    return 1;
+  else if (value < 0)
+    return -1;
+  else
+    return 0;
+}
+
+int calculate(double a, double b, double c, double roots[2], int accuracy) {
+  if (accuracy == 0) {
+    accuracy = 14;
+  }
+  if (a == 0) {
+    return -1;
+  }
+  double discriminant = b * b - 4 * a * c;
+  discriminant = get_accuracy(discriminant, 7);
+  if (discriminant < 0) {
+    return 0;
+  } else if (discriminant == 0) {
+    double x = -b / (2 * a);
+    roots[0] = get_accuracy(x, accuracy);
+    return 1;
+  } else {
+    double x1;
+    double x2;
+    if (b * b / fabs(4 * a * c) <= 10) {
+      x1 = (-b - sqrt(discriminant)) / (2 * a);
+      x2 = (-b + sqrt(discriminant)) / (2 * a);
+    } else {
+      x2 = -(b + sign(b) * sqrt(discriminant)) / 2 * a;
+      x1 = c / x2;
+    }
+    roots[0] = get_accuracy(x1, accuracy);
+    roots[1] = get_accuracy(x2, accuracy);
+    return 2;
+  }
+}
--- lab-1/tests/quadratic_test.c
+++ lab-1/tests/quadratic_test.c
@@ -1,3 +1,94 @@
 #include "../quadratic.h"
+#include <assert.h>
+#include <math.h>
+#define not_a_quadratic -1
+#define default_accuracy 14
 
-int main() { return 0; }
+int compare(double a, double b, double epsilon) {
+  if (epsilon == 0) {
+    epsilon = 1E-14;
+  }
+  return fabs(a - b) < epsilon;
+}
+
+void test1() {
+  double roots[2];
+  double a = 0;
+  double b = 0;
+  double c = -1;
+  int roots_count = calculate(a, b, c, roots, default_accuracy);
+  assert(roots_count == not_a_quadratic);
+}
+
+void test2() {
+  double roots[2];
+  double a = 1;
+  double b = 0;
+  double c = -1;
+  int roots_count = calculate(a, b, c, roots, default_accuracy);
+  assert(roots_count == 2);
+  assert(compare(roots[0], -1, 0));
+  assert(compare(roots[1], 1, 0));
+}
+
+void test3() {
+  double roots[2];
+  double a = 1;
+  double b = 0;
+  double c = 0;
+  int roots_count = calculate(a, b, c, roots, default_accuracy);
+  assert(roots_count == 1);
+  assert(compare(roots[0], 0, 0));
+}
+
+void test4() {
+  double roots[2];
+  double a = 1;
+  double b = 0;
+  double c = 1;
+  int roots_count = calculate(a, b, c, roots, default_accuracy);
+  assert(roots_count == 0);
+}
+
+void test5() {
+  double roots[2];
+  double a = 1;
+  double b = 0;
+  double c = -1E-7;
+  int roots_count = calculate(a, b, c, roots, 4);
+  assert(roots_count == 2);
+  assert(compare(roots[0], -3E-4, 1E-4));
+  assert(compare(roots[1], 3E-4, 1E-4));
+}
+
+void test6() {
+  double roots[2];
+  double a = 1;
+  double b = -1E+10;
+  double c = -1;
+  int roots_count = calculate(a, b, c, roots, 11);
+  assert(roots_count == 2);
+  assert(compare(roots[0], -1E-10, 1E-11));
+  assert(compare(roots[1], 1E+10, 1E-11));
+}
+
+void test7() {
+  double roots[2];
+  double a = 1;
+  double b = 0;
+  double c = -1E-8;
+  int roots_count = calculate(a, b, c, roots, 7);
+  assert(roots_count == 1);
+  assert(compare(roots[0], 0, 0));
+}
+
+int main() {
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
+  test6();
+  test7();
+  return 0;
+}
