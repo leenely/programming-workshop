59f7aaa1dc9655c3d07149899704bb2634a61ef5
leenely <yourflexium@protonmail.com>
2025-03-23 19:45:41 +0600

    feat: add full stack error output test

--- lab-1/tests/stack_test.c
+++ lab-1/tests/stack_test.c
@@ -15,20 +15,23 @@ void test_empty_stack() {
 
 void test_push() {
   stack stack;
+  int output;
   init(&stack, 5);
 
   push(&stack, 1);
-  int result = peek(&stack);
+  int result = peek(&stack, &output);
 
-  assert(result == 1);
+  assert(output == 1);
+  assert(result == SUCCESS_CODE);
   free(stack.array);
 }
 
 void test_empty_pop() {
   stack stack;
+  int output;
   init(&stack, 5);
 
-  int result = pop(&stack);
+  int result = pop(&stack, &output);
 
   assert(result == STACKUNDERFLOW_ERRCODE);
   free(stack.array);
@@ -36,36 +39,51 @@ void test_empty_pop() {
 
 void test_pop() {
   stack stack;
+  int output;
   init(&stack, 5);
 
   push(&stack, 1);
   push(&stack, 2);
-  pop(&stack);
-  int result = peek(&stack);
+  pop(&stack, &output);
+  int result = peek(&stack, &output);
 
-  assert(result == 1);
+  assert(output == 1);
+  assert(result == SUCCESS_CODE);
   free(stack.array);
 }
 
 void test_full_stack() {
   stack stack;
+  int output;
   init(&stack, 5);
 
   for (int i = 1; i <= stack.size + 1; i++) {
     push(&stack, i);
   }
-  int result = peek(&stack);
+  int result = peek(&stack, &output);
 
-  assert(result == 5);
+  assert(output == 5);
   assert(is_full(&stack) == true);
   free(stack.array);
 }
 
+void test_full_stack_error_output() {
+  stack stack;
+  int output;
+  init(&stack, 1);
+
+  push(&stack, 1);
+  int result = push(&stack, 2);
+
+  assert(result == STACKOVERFLOW_ERRCODE);
+}
+
 void test_empty_peek() {
   stack stack;
+  int output;
   init(&stack, 5);
 
-  int result = peek(&stack);
+  int result = peek(&stack, &output);
 
   assert(result == STACKUNDERFLOW_ERRCODE);
   free(stack.array);
@@ -77,6 +95,7 @@ int main() {
   test_empty_pop();
   test_pop();
   test_full_stack();
+  test_full_stack_error_output();
   test_empty_peek();
   return 0;
 }
