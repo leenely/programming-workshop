db84c36527383cc797d29ee45c5115090ce88b31
leenely <yourflexium@protonmail.com>
2025-03-23 19:01:20 +0600

    feat: add is_full

--- lab-1/stack.c
+++ lab-1/stack.c
@@ -1,34 +1,33 @@
 #include "stack.h"
+#include <stdbool.h>
 #include <stdlib.h>
 
 void init(stack *stack, int size) {
   stack->head = -1;
   stack->size = size;
   stack->array = (int *)malloc(sizeof(int) * size);
-  stack->is_empty = 1;
 }
 
-int is_empty(stack *stack) { return stack->is_empty; }
+bool is_empty(stack *stack) { return stack->head == -1; }
 
-void push(stack *stack, int value) {
-  if (stack->head + 1 >= stack->size) {
+bool is_full(stack *stack) { return stack->head == stack->size - 1; }
+
+int push(stack *stack, int value) {
+  if (is_full(stack)) {
     return STACKOVERFLOW_ERRCODE;
   }
 
   stack->head = stack->head + 1;
   stack->array[stack->head] = value;
-  stack->is_empty = 0;
 }
 
-void pop(stack *stack) {
+int pop(stack *stack) {
   if (is_empty(stack)) {
     return STACKUNDERFLOW_ERRCODE;
   }
 
   stack->head = stack->head - 1;
-  if (stack->head == -1) {
-    stack->is_empty = 1;
-  }
+  return stack->array[stack->head];
 }
 
 int peek(stack *stack) {
--- lab-1/stack.h
+++ lab-1/stack.h
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #define STACKOVERFLOW_ERRCODE -1
 #define STACKUNDERFLOW_ERRCODE -2
+
 typedef struct {
   int *array;
   int head;
@@ -10,10 +11,12 @@ typedef struct {
 
 void init(stack *stack, int size);
 
-int is_empty(stack *stack);
+bool is_empty(stack *stack);
 
-void push(stack *stack, int value);
+bool is_full(stack *stack);
 
-void pop(stack *stack);
+int push(stack *stack, int value);
+
+int pop(stack *stack);
 
 int peek(stack *stack);
