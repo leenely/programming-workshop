e7743f990e0958b841784252d5f9a3fc0f98c075
leenely <yourflexium@protonmail.com>
2025-04-25 19:42:46 +0600

    fix: arraylist_test and linear_alloc_test

--- lab-2/tests/arraylist_test.c
+++ lab-2/tests/arraylist_test.c
@@ -1,131 +1,34 @@
-#include "../arraylist.h"
-#include "../pool_alloc.h"
 #include <assert.h>
-#include <string.h>
+#include <stdio.h>
 
-void test_arraylist_init() {
-  PoolAllocator allocator;
-  pool_init(&allocator, sizeof(void *), 8);
+#include "../arraylist.h"
+#include "../linear_alloc.h"
 
+void run_simple_tests() {
   ArrayList list;
-  arraylist_init(&list, &allocator);
+  arraylist_init(&list, linear_init(1024));
 
-  assert(list.size == 0);
-  assert(list.capacity == 4);
-  assert(list.data != NULL);
-  assert(list.allocator == &allocator);
-
-  arraylist_free(&list);
-  pool_deinit(&allocator);
-}
-
-void test_arraylist_add() {
-  PoolAllocator allocator;
-  pool_init(&allocator, sizeof(void *), 8);
-
-  ArrayList list;
-  arraylist_init(&list, &allocator);
-
-  int a = 1, b = 2, c = 3;
+  int a = 10, b = 20, c = 30;
 
   arraylist_add(&list, &a, 0);
   arraylist_add(&list, &b, 1);
   arraylist_add(&list, &c, 2);
 
+  assert(*(int *)arraylist_get(&list, 0) == 10);
+  assert(*(int *)arraylist_get(&list, 1) == 20);
+  assert(*(int *)arraylist_get(&list, 2) == 30);
   assert(list.size == 3);
-  assert(*(int *)arraylist_get(&list, 0) == 1);
-  assert(*(int *)arraylist_get(&list, 1) == 2);
-  assert(*(int *)arraylist_get(&list, 2) == 3);
 
-  arraylist_free(&list);
-  pool_deinit(&allocator);
-}
-
-void test_arraylist_grow_capacity() {
-  PoolAllocator allocator;
-  pool_init(&allocator, sizeof(void *), 8);
-
-  ArrayList list;
-  arraylist_init(&list, &allocator);
-
-  int values[10];
-  for (int i = 0; i < 10; ++i) {
-    values[i] = i + 1;
-    arraylist_add(&list, &values[i], list.size);
-  }
-
-  assert(list.size == 10);
-  assert(list.capacity >= 10); // Минимум 10
-  for (int i = 0; i < 10; ++i) {
-    assert(*(int *)arraylist_get(&list, i) == i + 1);
-  }
-
-  arraylist_free(&list);
-  pool_deinit(&allocator);
-}
-
-void test_arraylist_delete() {
-  PoolAllocator allocator;
-  pool_init(&allocator, sizeof(void *), 8);
-
-  ArrayList list;
-  arraylist_init(&list, &allocator);
-
-  int a = 1, b = 2, c = 3;
-  arraylist_add(&list, &a, 0);
-  arraylist_add(&list, &b, 1);
-  arraylist_add(&list, &c, 2);
-
-  arraylist_del(&list, 1); // Удаляем b
+  arraylist_del(&list, 1);
   assert(list.size == 2);
-  assert(*(int *)arraylist_get(&list, 0) == 1);
-  assert(*(int *)arraylist_get(&list, 1) == 3);
+  assert(*(int *)arraylist_get(&list, 0) == 10);
+  assert(*(int *)arraylist_get(&list, 1) == 30);
 
   arraylist_free(&list);
-  pool_deinit(&allocator);
-}
-
-void test_arraylist_invalid_index() {
-  PoolAllocator allocator;
-  pool_init(&allocator, sizeof(void *), 8);
-
-  ArrayList list;
-  arraylist_init(&list, &allocator);
-
-  int a = 1;
-  arraylist_add(&list, &a, 0);
-
-  assert(arraylist_get(&list, 100) == NULL); // Выход за границы
-  arraylist_del(&list, 100); // Не должен изменить состояние
-
-  arraylist_free(&list);
-  pool_deinit(&allocator);
-}
-
-void test_arraylist_free_memory() {
-  PoolAllocator allocator;
-  pool_init(&allocator, sizeof(void *), 8);
-
-  ArrayList list;
-  arraylist_init(&list, &allocator);
-
-  int a = 1;
-  arraylist_add(&list, &a, 0);
-
-  arraylist_free(&list);
-  assert(list.data == NULL);
-  assert(list.size == 0);
-  assert(list.capacity == 0);
-
-  pool_deinit(&allocator);
+  linear_deinit(linear_init(1024));
 }
 
 int main() {
-  test_arraylist_init();
-  test_arraylist_add();
-  test_arraylist_grow_capacity();
-  test_arraylist_delete();
-  test_arraylist_invalid_index();
-  test_arraylist_free_memory();
+  run_simple_tests();
   return 0;
 }
