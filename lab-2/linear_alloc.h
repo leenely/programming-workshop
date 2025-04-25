2710f80df5aba6c5d5289f853b42f01f8aa5073d
leenely <yourflexium@protonmail.com>
2025-04-25 19:40:43 +0600

    feat: add linear allocator realization

--- lab-2/linear_alloc.h
+++ lab-2/linear_alloc.h
@@ -1,1 +1,12 @@
-// Linear-аллокатор
+#include <stddef.h>
+
+typedef struct {
+  size_t size;
+  size_t current_position;
+  void *memory;
+} linear_allocator;
+
+void *linear_init(size_t size);
+void *linear_alloc(linear_allocator *allocator, size_t size);
+void *linear_deinit(linear_allocator *allocator);
+void *linear_reset(linear_allocator *allocator);
