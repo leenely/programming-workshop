3575b0060159d85aa0b901bc550c838072cc0f52
Baklanov-Daniil <daniilbaklanov55@gmail.com>
2025-04-30 13:04:05 +0600

    fix: changed value2 in 1test

--- lab-2/tests/hashtable_test.c
+++ lab-2/tests/hashtable_test.c
@@ -2,30 +2,27 @@
 #include "../pool_alloc.h"
 #include <assert.h>
 #include <stdio.h>
-#include <string.h>
 
 void test_basic_insert_get() {
-
   PoolAllocator alloc;
   pool_init(&alloc, sizeof(HashElem), 10);
   HashTable table;
   hashtable_init(&table, 5, &alloc);
 
   int value1 = 42;
-  float value2 = 3.14f;
+  char value2 = 'a';
 
   hashtable_insert(&table, "answer", &value1);
-  hashtable_insert(&table, "pi", &value2);
+  hashtable_insert(&table, "char", &value2);
 
   assert(*(int *)hashtable_get(&table, "answer") == 42);
-  assert(*(float *)hashtable_get(&table, "pi") == 3.14f);
+  assert(*(char *)hashtable_get(&table, "char") == 'a');
 
   hashtable_free(&table);
   pool_deinit(&alloc);
 }
 
 void test_collision_handling() {
-
   PoolAllocator alloc;
   pool_init(&alloc, sizeof(HashElem), 10);
   HashTable table;
@@ -37,15 +34,14 @@ void test_collision_handling() {
   hashtable_insert(&table, str1, str1);
   hashtable_insert(&table, str2, str2);
 
-  assert(strcmp(hashtable_get(&table, str1), str1) == 0);
-  assert(strcmp(hashtable_get(&table, str2), str2) == 0);
+  assert(hashtable_get(&table, str1) == str1);
+  assert(hashtable_get(&table, str2) == str2);
 
   hashtable_free(&table);
   pool_deinit(&alloc);
 }
 
 void test_update_value() {
-
   PoolAllocator alloc;
   pool_init(&alloc, sizeof(HashElem), 5);
   HashTable table;
@@ -64,7 +60,6 @@ void test_update_value() {
 }
 
 void test_delete() {
-
   PoolAllocator alloc;
   pool_init(&alloc, sizeof(HashElem), 5);
   HashTable table;
@@ -86,6 +81,5 @@ int main() {
   test_collision_handling();
   test_update_value();
   test_delete();
-
   return 0;
 }
