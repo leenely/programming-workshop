f12bc7f14acd91163c2e9c002364b9167051168a
dexffe <dexffe@gmail.com>
2025-05-10 16:13:11 +0600

    fix: corrected code

--- lab-2/arraylist.c
+++ lab-2/arraylist.c
@@ -5,53 +5,60 @@
 
 static void arraylist_grow(ArrayList *list) {
   size_t new_capacity = list->capacity * 2;
-  void **new_data =
-      linear_alloc(list->allocator, sizeof(void *) * new_capacity);
+  char *new_data =
+      linear_alloc(list->allocator, list->element_size * new_capacity);
   assert(new_data && "Failed to grow array");
 
-  memcpy(new_data, list->data, sizeof(void *) * list->size);
+  memcpy(new_data, list->data, list->element_size * list->length);
   list->data = new_data;
   list->capacity = new_capacity;
 }
 
-void arraylist_init(ArrayList *list, linear_allocator *allocator) {
+void arraylist_init(ArrayList *list, linear_allocator *allocator,
+                    size_t element_size) {
   list->allocator = allocator;
   list->capacity = 4;
-  list->size = 0;
+  list->length = 0;
+  list->element_size = element_size;
 
-  list->data = linear_alloc(allocator, sizeof(void *) * list->capacity);
+  list->data = linear_alloc(allocator, element_size * list->capacity);
   assert(list->data && "Failed to allocate initial array");
 }
 
 void arraylist_add(ArrayList *list, void *element, size_t index) {
-  assert(index <= list->size && "Index out of bounds");
+  assert(index <= list->length && "Index out of bounds");
 
-  if (list->size == list->capacity) {
+  if (list->length == list->capacity) {
     arraylist_grow(list);
   }
 
-  memmove(list->data + index + 1, list->data + index,
-          sizeof(void *) * (list->size - index));
-  list->data[index] = element;
-  list->size++;
+  memmove(list->data + (index + 1) * list->element_size,
+          list->data + index * list->element_size,
+          (list->length - index) * list->element_size);
+
+  memcpy(list->data + index * list->element_size, element, list->element_size);
+  list->length++;
 }
 
 void *arraylist_get(ArrayList *list, size_t index) {
-  assert(index < list->size && "Index out of bounds");
-  return list->data[index];
+  assert(index < list->length && "Index out of bounds");
+  return list->data + index * list->element_size;
 }
 
 void arraylist_del(ArrayList *list, size_t index) {
-  assert(index < list->size && "Index out of bounds");
+  assert(index < list->length && "Index out of bounds");
 
-  memmove(list->data + index, list->data + index + 1,
-          sizeof(void *) * (list->size - index - 1));
-  list->size--;
+  memmove(list->data + index * list->element_size,
+          list->data + (index + 1) * list->element_size,
+          (list->length - index - 1) * list->element_size);
+
+  list->length--;
 }
 
 void arraylist_free(ArrayList *list) {
   list->data = NULL;
-  list->size = 0;
+  list->length = 0;
   list->capacity = 0;
+  list->element_size = 0;
   list->allocator = NULL;
 }
--- lab-2/arraylist.h
+++ lab-2/arraylist.h
@@ -4,13 +4,15 @@
 #include "linear_alloc.h"
 
 typedef struct {
-  void **data;
-  size_t size;
+  void *data;
+  size_t length;
   size_t capacity;
+  size_t element_size;
   linear_allocator *allocator;
 } ArrayList;
 
-void arraylist_init(ArrayList *list, linear_allocator *allocator);
+void arraylist_init(ArrayList *list, linear_allocator *allocator,
+                    size_t element_size);
 void arraylist_add(ArrayList *list, void *element, size_t index);
 void *arraylist_get(ArrayList *list, size_t index);
 void arraylist_del(ArrayList *list, size_t index);
