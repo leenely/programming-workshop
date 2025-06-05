332cdd652ecdf5a85c4b87740f7786ba02aa2b78
leenely <yourflexium@protonmail.com>
2025-06-05 23:44:53 +0600

    feat: add tests for destructors

--- garbage_collection/tests/destructor_test.c
+++ garbage_collection/tests/destructor_test.c
@@ -1,4 +1,57 @@
-// Деструкторы для объектов
-#include "../destructor.h"
+#include "../garbage_collector.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() { return 0; }
+void test_ref_count_create_without_destructor() {
+  ref_counting_init(10);
+  int *obj = malloc(sizeof(int));
+  *obj = 10;
+
+  ref_count_t *rc = ref_count_create(obj, NULL);
+  assert(rc != NULL);
+  assert(rc->count == 1);
+  assert(rc->object == obj);
+  assert(rc->destructor == NULL);
+
+  ref_count_dec(rc);
+  ref_counting_deinit();
+}
+
+void custom_destructor(void *ptr) {
+  printf("Кастомный деструктор отработал\n");
+  free(ptr);
+}
+
+void test_ref_count_create_with_custom_destructor() {
+  ref_counting_init(10);
+  int *obj = malloc(sizeof(int));
+  *obj = 10;
+
+  ref_count_t *rc = ref_count_create(obj, custom_destructor);
+  assert(rc != NULL);
+  assert(rc->count == 1);
+  assert(rc->object == obj);
+  assert(rc->destructor != NULL);
+
+  ref_count_dec(rc);
+  ref_counting_deinit();
+}
+
+void test_ref_count_create_with_custom_destructor_and_null_object() {
+  ref_counting_init(10);
+  ref_count_t *rc = ref_count_create(NULL, custom_destructor);
+
+  assert(rc == NULL);
+
+  ref_count_dec(rc);
+  ref_counting_deinit();
+}
+
+int main() {
+  test_ref_count_create_without_destructor();
+  test_ref_count_create_with_custom_destructor();
+  test_ref_count_create_with_custom_destructor_and_null_object();
+
+  return 0;
+}
